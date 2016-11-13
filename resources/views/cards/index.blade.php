ccb1af636c04d891e1ad7f539a54308c6a8ee1a0
Ravneet <ravneetrahi12@gmail.com>
2016-11-13 23:32:19 +0530

    Updated 2 files

--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -13,14 +13,14 @@
 
 <h3>Add Card</h3>
 <form method="POST" action="/card">
-	<div class="form-group">
-		<input type="text" name="title" class="form-control" placeholder="Title" value="{{old('title')}}">		
+	<div>
+		<input type="text" name="title" placeholder="Title" value="{{old('title')}}">		
 	</div>
-	<div class="form-group">
-		<textarea name="description" class="form-control mb10" placeholder="Description"></textarea>
+	<div>
+		<textarea name="description"  placeholder="Description"></textarea>
 	</div>
-	<div class="form-group">
-		<input type="submit" class="btn btn-primary" value="Add New Card">
+	<div>
+		<input type="submit" value="Add New Card">
 	</div>
 </form>	
 @if(count($errors))
